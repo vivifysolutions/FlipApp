87aabdacf3f1c9ada141e8e132ea11679f5021f1
DanMainaChiwaz <daniel.maina@vivifysolutions.net>
2024-01-15 22:29:39 +0300

    FLIP-2-backend-scaffolding added the firebase, event, auth folders

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EventController } from './event.controller';
+import { EventService } from './event.service';
 
-@Module({})
+@Module({
+  controllers: [EventController],
+  providers: [EventService]
+})
 export class EventModule {}
