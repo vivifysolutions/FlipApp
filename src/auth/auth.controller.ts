fa66b2aaf6da87ad69a526bdf0ca63fe34b94176
DanMainaChiwaz <daniel.maina@vivifysolutions.net>
2024-01-16 23:56:03 +0300

    FLIP-2-backend-scaffolding added the events model

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){}
+
+    signUp(){
+    }
+
+    signIn(){}
+
+    googleLogin(){}
+
+    facebookLogin(){}
+
+    twitterLogin(){}
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+
+
+@Global()
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+   exports:[AuthService]
 })
 export class AuthModule {}
